9b6a8062bed1f90f98d30d7fa12520ca77058cc5
USER <baronelismy@gmail.com>
2020-01-09 01:54:11 -0300

    modificacion de preco api no funciona

--- src/components/Global/Preco.js
+++ src/components/Global/Preco.js
@@ -3,7 +3,7 @@ import React,{Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/Preco.css';
 import {Col,Form, Row,Card,ListGroup,ListGroupItem,Button,ButtonToolbar} from 'react-bootstrap';
-import carro from './carro.json';
+
 //import axios from 'axios';
 //import {useState,useEffect} from 'react';
 
@@ -12,71 +12,107 @@ class Preco extends Component{
   constructor(props){
     super(props);
     this.state={
-      items:[],
+      marcas:[],
+      modelos:[],
+      anos:[],
+      showresult:[],
+      loading:false
     
-      isLoaded:false,
-
     }
   }
 
-  componentCarro(){
-    //aqui en el fetch va o endereço da api
-    fetch('./carro.json')
-     .then(res => res.json())
-     .then(json =>{
-       this.setState({
-         isLoaded:true,
-         items:json,
-        
+  componentDidMount(){
+       this.getMarcas();
+       this.getModelos();
+       this.getAnos();
+       }
 
-       })
-     } );
-  }
+       getMarcas = () => {
+         this.setState ({ loading:true })
+         fetch('http://leonardodavila-price-api.glitch.me/brands')
+         .then(res =>res.json()) 
+         .then( resmarcas => {
+            this.setState({
+            marcas:resmarcas,
+             loading:false
+           })
+           console.log(resmarcas)
+         })
+
+       }
+       getModelos = () =>{
+        this.setState ({ loading:true })
+        fetch('http://leonardodavila-price-api.glitch.me/brands/:brand/models')
+        .then(res =>res.json()) 
+         .then( resmodelos => {
+           this.setState({
+             modelos:resmodelos,
+             loading:false
+           })
+         })
+       };
+       getAnos = () =>{
+        this.setState ({ loading:true })
+        fetch('http://leonardodavila-price-api.glitch.me/brands/:brand/models/:model/years')
+        .then(res =>res.json()) 
+         .then( resanos => {
+           this.setState({
+             anos:resanos,
+             loading:false
+           })
+         })
+       };
+     
+  
  
   render(){
-    var {isLoaded, items,item }= this.state;
-    if(!isLoaded){
-      return <div> Loading.....</div>
-       }
-       else{
-  //GET: http://fipeapi.appspot.com/api/1/carros/marcas.json
-  //GET: http://fipeapi.appspot.com/api/1/carros/veiculos/21.json
-  //GET: http://fipeapi.appspot.com/api/1/carros/veiculo/21/4828.json
-  //GET: http://fipeapi.appspot.com/api/1/carros/veiculo/21/4828/2013-1.json
-   
-    return(
+    var{marcas, modelos, anos,showresult}= this.state;
+ 
     
+    return(
+     
       <div className="Formulario">
            <Form className="select-form">
                <Row>
                    <Col xs={12} md={3}>
-                     {items.map(item =>(
-                     <select className="form-control bg-primary" >
-                        <option  key={item.id}>Marca: {item.marca}</option>
-                     </select>
-                     ))}
+                    
+                    <select className="form-control bg-primary" > 
+                    {marcas.map(marcas =>{
+                     return  <option key={marcas.id} >Marca:{marcas}</option>
+                     showresult.push({marcas})
+                     })}
+                       </select>
+                   </Col>
+
+
+                   <Col xs={12} md={3}>
+                  
+                   <select className="form-control bg-primary" > 
+                    {modelos.map(modelos =>{
+                     return  <option key={modelos.id} >Modelo:{modelos}</option>
+                     showresult.push({modelos})
+                     })}
+                       </select>
+                      
                    </Col>
                    <Col xs={12} md={3}>
-                   {items.map(item =>(
-                     <select className="form-control bg-primary" >
-                       <option  key={item.id}>Modelo: {item.modelo}</option>
-                     </select>
-                      ))}
+                  
+                   <select className="form-control bg-primary" > 
+                    {anos.map(anos =>{
+                     return  <option key={anos.id} >Ano:{anos}</option>
+                     showresult.push({anos })
+                     })}
+                       </select>
+                      
                    </Col>
                    <Col xs={12} md={3}>
-                      {items.map(item =>(
-                     <select className="form-control bg-primary" >
-                       <option  key={item.id}>Ano: {item.ano}</option>
-                     </select>
-                      ))}
-                   </Col>
-                   <Col xs={12} md={3}>
-                      {items.map(item =>(
+                    
                      <Form.Control as="select" className="form-control bg-primary">
-                      <option  key={item.id}>Estado: {item.estado}</option>
-                         
+                      <option>Estado:</option>
+                      <option >Rio de Janeiro</option>
+                      <option >São Paulo</option>
                      </Form.Control>
-                      ))}
+                     
                    </Col>
              </Row>
              <ButtonToolbar>
@@ -88,26 +124,27 @@ class Preco extends Component{
                  <Card.Header>Autos</Card.Header>
                <Card.Body>
                <Card.Title>Descripção</Card.Title>
-                 <Card.Text
-                  key={item.id}>Descripção:{item.descripcao}
+                 <Card.Text>Descripção:
                   </Card.Text>
                   <ListGroup className="list-group-flush">
-                      <ListGroupItem key={item.precio}>Precio:{item.precio}</ListGroupItem>
-                               <ListGroupItem key={item.id}>Marca:{item.marca}</ListGroupItem>
-                               <ListGroupItem key={item.id}>Modelo:{item.modelo}</ListGroupItem>
-                               <ListGroupItem key={item.id}>Ano:{item.ano}</ListGroupItem>
-                               <ListGroupItem key={item.id}>Combustível:{item.combustivel}</ListGroupItem>
+                      <ListGroupItem>Precio:</ListGroupItem>
+                      {showresult.map(showresult =>{
+                      return <ListGroupItem key={showresult.marcas} >Marca:{showresult}</ListGroupItem>})}
+                       {showresult.map(showresult =>{
+                         return <ListGroupItem key={showresult.modelos}>Modelo:{showresult}</ListGroupItem>})}
+                         {showresult.map(showresult =>{
+                            return    <ListGroupItem key={showresult.anos} >Ano:{showresult}</ListGroupItem>})}
+                             
                          </ListGroup>
                </Card.Body>
              </Card>
              </div>
     
-
-
     );
-  }
+                    }
+                  }
+                  
 
-  }
-}
+
      
 export default Preco;
