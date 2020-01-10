f95761563b81a7a5e07d028be9fc33f854f91e0d
USER <baronelismy@gmail.com>
2020-01-10 07:27:33 -0300

    Doesnt call to models and yaers

--- src/components/Global/Preco.js
+++ src/components/Global/Preco.js
@@ -15,60 +15,104 @@ class Preco extends Component{
       marcas:[],
       modelos:[],
       anos:[],
+      version:[],
+      carros:[],
       showresult:[],
       loading:false
     
     }
+    this.myrefmarca=React.createRef();
+    this.myrefano=React.createRef();
+    this.myrefmodelo=React.createRef();
+    this.myrefversion=React.createRef();
+
   }
 
   componentDidMount(){
        this.getMarcas();
        this.getModelos();
        this.getAnos();
+       this.getVersion();
+      // this.getCarros(); ta com erro no fetch
        }
 
        getMarcas = () => {
-         this.setState ({ loading:true })
-         fetch('http://leonardodavila-price-api.glitch.me/brands')
+        this.setState ({ loading:true })
+         fetch('https://volanty-price-api.herokuapp.com/brands')
          .then(res =>res.json()) 
          .then( resmarcas => {
             this.setState({
             marcas:resmarcas,
-             loading:false
+             loading:false,
            })
            console.log(resmarcas)
-         })
+         }) 
 
        }
        getModelos = () =>{
         this.setState ({ loading:true })
-        fetch('http://leonardodavila-price-api.glitch.me/brands/:brand/models')
+        const marca=this.myrefmarca.current;//Lucas Fiz uma referencia pois no react não posso utilizar o document.getElementById
+        fetch('https://volanty-price-api.herokuapp.com/brands/' + marca +'/models')
         .then(res =>res.json()) 
          .then( resmodelos => {
            this.setState({
              modelos:resmodelos,
              loading:false
            })
+           console.log(resmodelos)
          })
+        
        };
        getAnos = () =>{
         this.setState ({ loading:true })
-        fetch('http://leonardodavila-price-api.glitch.me/brands/:brand/models/:model/years')
+        var marca=this.myrefmarca.current;
+        var modelo=this.myrefmodelo.current;
+        fetch('http://volanty-price-api.herokuapp.com/brands/'+ marca +'/models/'+modelo+'/years')
         .then(res =>res.json()) 
          .then( resanos => {
            this.setState({
              anos:resanos,
              loading:false
            })
+           console.log(resanos)
          })
        };
+       getVersion = () =>{
+        this.setState ({ loading:true })
+        var marca=this.myrefmarca.current;
+        var modelo=this.myrefmodelo.current;
+        var ano=this.myrefano.current;
+        fetch('http://volanty-price-api.herokuapp.com/brands/'+ marca +'/models/'+modelo+'/years/' + ano +'/versions')
+        .then(res =>res.json()) 
+         .then( resversion => {
+           this.setState({
+             version:resversion,
+             loading:false
+           })
+           console.log(resversion)
+         })
+       };
+       /*getCarros = () =>{
+        this.setState ({ loading:true })
+        var marca=this.myrefmarca.current;
+        var modelo=this.myrefmodelo.current;
+        var ano=this.myrefano.current;
+        var version=this.myrefversion;
+        fetch('http://volanty-price-api.herokuapp.com/brands/'+ marca +'/models/'+ modelo +'/years/' + ano +'/versions/' + version)
+        .then(res =>res.json()) 
+         .then( rescarro => {
+           this.setState({
+             carros:rescarro,
+             loading:false
+           })
+           console.log(rescarro)
+         })
+       };*/
      
   
  
   render(){
-    var{marcas, modelos, anos,showresult}= this.state;
- 
-    
+    var{marcas, modelos, anos,showresult,version, carros}= this.state;
     return(
      
       <div className="Formulario">
@@ -76,35 +120,44 @@ class Preco extends Component{
                <Row>
                    <Col xs={12} md={3}>
                     
-                    <select className="form-control bg-primary" > 
-                    {marcas.map(marcas =>{
-                     return  <option key={marcas.id} >Marca:{marcas}</option>
-                     showresult.push({marcas})
+                    <select className="form-control bg-primary" id="marcas" onChange={this.getModelos}> 
+                    {marcas.map((marca) =>{//esta e a sintaxis do for no react, entao na option coloquei a referencia
+                      return <option key={marca}  ref={this.myrefmarca}>Marca: {marca}</option>
+                  
                      })}
                        </select>
                    </Col>
-
-
-                   <Col xs={12} md={3}>
-                  
-                   <select className="form-control bg-primary" > 
+                   <Col xs={12} md={3}>         
+                   <select className="form-control bg-primary" id="modelos" onChange={this.getAnos}> 
                     {modelos.map(modelos =>{
-                     return  <option key={modelos.id} >Modelo:{modelos}</option>
-                     showresult.push({modelos})
+                     return  <option key={modelos} ref={this.myrefmodelo}>Modelo: {modelos}</option>
+                   
                      })}
                        </select>
                       
                    </Col>
                    <Col xs={12} md={3}>
                   
-                   <select className="form-control bg-primary" > 
+                   <select className="form-control bg-primary" id="anos" onChange={this.getVersion}> 
                     {anos.map(anos =>{
-                     return  <option key={anos.id} >Ano:{anos}</option>
-                     showresult.push({anos })
+                     return  <option key={anos} ref={this.myrefano}>Ano: {anos}</option>
+                  
                      })}
                        </select>
                       
                    </Col>
+                   <Col xs={12} md={3}>
+                  
+                   <select className="form-control bg-primary" id="version" onChange={this.getCarros}> 
+                    {version.map(version =>{
+                     return  <option key={version} ref={this.myrefversion}>Version: {version}</option>
+                  
+                     })}
+                       </select>
+                      
+                   </Col>
+
+                 
                    <Col xs={12} md={3}>
                     
                      <Form.Control as="select" className="form-control bg-primary">
@@ -116,7 +169,7 @@ class Preco extends Component{
                    </Col>
              </Row>
              <ButtonToolbar>
-             <Button className="calcular mt-3" type="submit">Calcular Agora</Button>
+             <Button className="calcular mt-3" type="submit">Calcular Preço</Button>
              </ButtonToolbar>
            </Form>
 
@@ -128,12 +181,12 @@ class Preco extends Component{
                   </Card.Text>
                   <ListGroup className="list-group-flush">
                       <ListGroupItem>Precio:</ListGroupItem>
-                      {showresult.map(showresult =>{
-                      return <ListGroupItem key={showresult.marcas} >Marca:{showresult}</ListGroupItem>})}
-                       {showresult.map(showresult =>{
-                         return <ListGroupItem key={showresult.modelos}>Modelo:{showresult}</ListGroupItem>})}
-                         {showresult.map(showresult =>{
-                            return    <ListGroupItem key={showresult.anos} >Ano:{showresult}</ListGroupItem>})}
+                      {carros.map(carro =>{
+                      return <ListGroupItem key={carro.marcas} >Marca:{carro.marcas}</ListGroupItem>})}
+                       {carros.map(carro =>{
+                         return <ListGroupItem key={carro.modelos}>Modelo:{carro.modelos}</ListGroupItem>})}
+                         {carros.map(carro =>{
+                            return    <ListGroupItem key={carro.anos} >Ano:{carro.anos}</ListGroupItem>})}
                              
                          </ListGroup>
                </Card.Body>
